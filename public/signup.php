0ea30c6263deab5f2972e7e0f7c12a515019d4a3
Ohanz <chiaguguadaniel@gmail.com>
2024-12-20 02:02:14 +0100

    added smart error side-pop

--- public/signup.php
+++ public/signup.php
@@ -6,12 +6,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
     $confirm_password = $_POST['confirm_password'];
+    $allErr = $nameErr = $emailErr = $passErr = "";
 
     // Validate user input
-    if (empty($username) || empty($email) || empty($password) || empty($confirm_password)) {
-        echo 'Please fill in all fields.';
-    } elseif ($password != $confirm_password) {
-        echo 'Passwords do not match.';
+    // if (empty($username) || empty($email) || empty($password) || empty($confirm_password)) {
+    //     $allErr= '* Please fill in all fields.';
+    // } 
+    if (empty($username) && empty($email) && empty($password) && empty($confirm_password)) {
+        $allErr= '* Please fill in all fields.';
+    }
+ elseif (empty($username)){
+   $nameErr = " * Hyper Name is required.";
+}
+elseif (empty($email)){
+    $emailErr = " * Email is required.";
+ }
+ elseif (empty($password)){
+    $passErr = " * Enter password.";
+ }
+    elseif ($password != $confirm_password) {
+        // echo 'Passwords do not match.';
+        $passErr = ' * Passwords do not match.';
     } else {
         // Check if username or email already exists
         $query = "SELECT * FROM hypers WHERE username = '$username' OR email = '$email'";
@@ -38,14 +53,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <link rel="stylesheet" href="style.css">
     </head>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+<p><span class="error-not">* Required field!</span></p>
+<div class="errorHead"><?php echo $allErr;?></div><br/>
     <label for="username">Username:</label>
-    <input type="text" id="username" name="username"><br><br>
+    <input type="text" id="username" value="<?php if(isset($username)) echo $username; ?>" name="username">
+    <span class="error"><?php echo $nameErr;?></span><br><br>
     <label for="email">Email:</label>
-    <input type="email" id="email" name="email"><br><br>
+    <input type="email" value="<?php if(isset($email)) echo $email; ?>" id="email" name="email">
+    <span class="error"><?php echo $emailErr;?></span><br><br>
     <label for="password">Password:</label>
-    <input type="password" id="password" name="password"><br><br>
+    <input type="password" id="password" name="password"><span class="error"><?php echo $passErr;?></span><br><br>
     <label for="confirm_password">Confirm Password:</label>
-    <input type="password" id="confirm_password" name="confirm_password"><br><br>
+    <input type="password" id="confirm_password" name="confirm_password">
+    <br><br>
     <input type="submit" value="Sign Up">
     <a href="login.php"><input type="button" name="logins" value="Login Instead"></a>
 </form>
