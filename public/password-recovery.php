933a5eb3ad618c4b00da29d3c53dfadde938266d
Ohanz <chiaguguadaniel@gmail.com>
2024-12-24 04:48:48 +0100

    added expiration timer to reset-token

--- public/password-recovery.php
+++ public/password-recovery.php
@@ -17,11 +17,27 @@ if (isset($_POST['recover'])) {
   if ($result->num_rows > 0) {
       // Generate password reset token
       $token = bin2hex(random_bytes(16));
+      // added expiration-var
+      $expires = date('Y-m-d H:i:s', strtotime('+30 minutes'));
 
       // Update user's password reset token in database
-      $stmt = $conn->prepare("UPDATE hypers SET password_reset_token = ? WHERE email = ?");
-      $stmt->bind_param("ss", $token, $email);
+      $stmt = $conn->prepare("UPDATE hypers SET password_reset_token = ?, password_reset_token_expires = ? WHERE email = ?");
+     $stmt->bind_param("sss", $token, $expires, $email);
       $stmt->execute();
+
+      $stmt = $conn->prepare("SELECT password_reset_token_expires FROM hypers WHERE email = ?");
+      $stmt->bind_param("s", $email);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      $expires = $result->fetch_assoc()['password_reset_token_expires'];
+  
+      if (strtotime($expires) < time()) {
+          echo "Token has expired. Please request a new password recovery token.";
+     // Redirect to request new token
+     header("Location: password-recovery.php");
+     exit;
+        } else {
+          // Proceed with password reset
 
       // Send password recovery email
       require '../vendor/autoload.php';
@@ -31,10 +47,10 @@ if (isset($_POST['recover'])) {
           $mail->SMTPDebug = 0;
           $mail->DebugOutput = '';
           $mail->isSMTP();
-          $mail->Host = 'smtp host';
+          $mail->Host = 'smtp.gmail.com';
           $mail->SMTPAuth = true;
-          $mail->Username = 'Smtp Email username';
-          $mail->Password = 'Smtp Email password';
+          $mail->Username = 'smtp email';
+          $mail->Password = 'smtp pass';
           $mail->SMTPSecure = 'tls';
           $mail->Port = 587;
 
@@ -52,6 +68,9 @@ if (isset($_POST['recover'])) {
       } catch (Exception $e) {
           echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
       }
+
+      }
+
   } else {
       echo "Email not found!";
   }
